2feb892a84d7c2e1e4e4653350769a213b12454e
Sourish Banerjee <sourish.banerjee2015@vit.ac.in>
2017-11-03 12:34:10 +0530

    Clean up output and add comments

--- CUDA_FF_Flow/bfs.cu
+++ CUDA_FF_Flow/bfs.cu
@@ -1,8 +1,3 @@
-// This is a parallel Breath First Search (BFS) Implementation using CUDA
-// Input: randomly generated graph in form of adjacent matrix stored in 'mygraph.txt'
-// BFS starts from node 0 and stops when the logical BFS tree is formed
-// usage: ./<out> <graph_size> <mygraph.txt
-
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
@@ -10,60 +5,62 @@
 
 using namespace std;
 
-__global__ void kernel(bool* adj_mat, const int N, bool* visited, int* frontier, bool* new_frontier, bool* par_mat, int* cap_mat, int* cap_max_mat){
+//Check for edges valid to be part of augmented path
+__global__ void kernel(bool* adj_mat, const int N, bool* visited, int* frontier, bool* new_frontier, bool* par_mat, int* cap_mat, int* cap_max_mat) {
 	int row_idx = frontier[blockIdx.x+1];
 	long offset = N * row_idx;
 
-	// update new_frontier in threads
 	int col_idx = threadIdx.x;
 	long offset2 = N * col_idx;
-	if(adj_mat[offset + col_idx] && (cap_mat[offset + col_idx] < cap_max_mat[offset + col_idx]) && !visited[col_idx]){
+	if(adj_mat[offset + col_idx] && (cap_mat[offset + col_idx] < cap_max_mat[offset + col_idx]) && !visited[col_idx]) {
 		new_frontier[col_idx] = true;
 		par_mat[offset2 + row_idx] = true;
 	}
 
-	if(adj_mat[offset2 + row_idx] && (cap_mat[offset2 + row_idx] > 0) && !visited[col_idx]){
+	if(adj_mat[offset2 + row_idx] && (cap_mat[offset2 + row_idx] > 0) && !visited[col_idx]) {
 		new_frontier[col_idx] = true;
 		par_mat[offset2 + row_idx] = true;
 	}
 }
-__global__ void k2(const int N, bool* visited, int* frontier, bool* new_frontier){
-	int cn = 0;
-	for(int i=0;i<N;i++){
-		if(new_frontier[i]){
+
+//Update frontier
+__global__ void k2(const int N, bool* visited, int* frontier, bool* new_frontier) {
+	int count = 0;
+	for(int i=0;i<N;i++) {
+		if(new_frontier[i]) {
 			new_frontier[i] = false;
-			frontier[++cn] = i;
+			frontier[++count] = i;
 			visited[i] = true;
 		}
 	}
-	frontier[0] = cn;
+	frontier[0] = count;
 }
 
-std::vector<std::string> split(std::string str,std::string sep){
+std::vector<std::string> split(std::string str,std::string sep) {
     char* cstr=const_cast<char*>(str.c_str());
     char* current;
     std::vector<std::string> arr;
     current=strtok(cstr,sep.c_str());
-    while(current!=NULL){
+    while(current!=NULL) {
         arr.push_back(current);
         current=strtok(NULL,sep.c_str());
     }
     return arr;
 }
 
-int main(int arg, char** argv){
-	if(arg!=2){
-		cout<<"usage: ./<out> <graph_size> <mygraph.txt"<<endl;
+int main(int arg, char** argv) {
+	if(arg!=2) {
+		cout<<"Please run in the following manner: ./<out> <graph_size> <<input>.txt"<<endl;
 		return -1;
 	}
 	const int N = atoi(argv[1]);
 
-	//read graph from <input>.txt
+	//Read graph from <input>.txt
 	bool* h_adj_mat = (bool*)malloc(N*N*sizeof(bool));
 	int* h_cap_mat = (int*)malloc(N*N*sizeof(int));
 	int* h_cap_max_mat = (int*)malloc(N*N*sizeof(int));
 
-	for(int i=0;i<N*N;i++){
+	for(int i=0;i<N*N;i++) {
 		string a;
 		cin>>a;
 
@@ -83,20 +80,10 @@ int main(int arg, char** argv){
 	for(int i=0;i<N*N;i++)
 		h_par_mat[i] = false;
 
-	//generate visited and frontier vector; init them with node 0;
-	bool* h_visited = (bool*)malloc(N*sizeof(bool));
-	for(int i=0;i<N;i++) h_visited[i] = false;
-	int* h_frontier = (int*)malloc(N*sizeof(int));
-	bool* h_new_frontier = (bool*)malloc(N*sizeof(bool));
-	for(int i=0;i<N;i++) h_new_frontier[i] = false;
-
-	h_visited[0] = true;
-	h_frontier[0] = 1;
-	h_frontier[1] = 0;
-
-	//malloc mem in gpu
-	clock_t start,end, s, e;
+	clock_t start, end, s, e;
 	start = clock();
+
+	//Allocate device memory for adj_mat, cap_mat and cap_max_mat
 	bool *d_adj_mat, *d_par_mat, *d_visited, *d_new_frontier;
 	int *d_cap_mat, *d_cap_max_mat, *d_frontier;
 	cudaMalloc((void**) &d_adj_mat, sizeof(bool) * N * N);
@@ -108,15 +95,14 @@ int main(int arg, char** argv){
 	cudaMalloc((void**) &d_cap_max_mat, sizeof(int) * N * N);
 	cudaMemcpy((void*) d_cap_max_mat, (void*) h_cap_max_mat, sizeof(int)*N*N, cudaMemcpyHostToDevice);
 
-	//loop until frontier vector is empty
-	int cn=1;
-	double t=0;
+	double t = 0;
 	while(1) {
+		//Generate and initialize par_mat
 		bool* h_par_mat = (bool*)malloc(N*N*sizeof(bool));
 		for(int i=0;i<N*N;i++)
 			h_par_mat[i] = false;
 
-		//generate visited and frontier vector; init them with node 0;
+		//Generate and initialize visited and frontier
 		bool* h_visited = (bool*)malloc(N*sizeof(bool));
 		for(int i=0;i<N;i++) h_visited[i] = false;
 		int* h_frontier = (int*)malloc(N*sizeof(int));
@@ -127,54 +113,55 @@ int main(int arg, char** argv){
 		h_frontier[0] = 1;
 		h_frontier[1] = 0;
 
+		//Allocate device memory for par_mat, visited and frontier
 		cudaMalloc((void**) &d_par_mat, sizeof(bool) * N * N);
 		cudaMemcpy((void*) d_par_mat, (void*) h_par_mat, sizeof(bool)*N*N, cudaMemcpyHostToDevice);
 
 		cudaMalloc((void**) &d_visited, sizeof(bool) * N);
 		cudaMemcpy((void*) d_visited, (void*) h_visited, sizeof(bool)*N, cudaMemcpyHostToDevice);
 
-		cudaMalloc((void**) &d_frontier, sizeof(int) * (N+1));
+		cudaMalloc((void**) &d_frontier, sizeof(int) * (N + 1));
 		cudaMemcpy((void*) d_frontier, (void*) h_frontier, sizeof(int)*N, cudaMemcpyHostToDevice);
 
 		cudaMalloc((void**) &d_new_frontier, sizeof(bool) * N);
 		cudaMemcpy((void*) d_new_frontier, (void*) h_new_frontier, sizeof(bool)*N, cudaMemcpyHostToDevice);
 
 		bool augFound = false;
-		while(h_frontier[0]!=0){
-			cn+=h_frontier[0];
+		while(h_frontier[0] != 0) {
 
+			cout<<"Frontier:"<<endl<<endl;
 			for(int i = 0; i < h_frontier[0]; i++)
 				cout<<h_frontier[i + 1]<<" ";
 			cout<<endl;
 
+			//Complete search if sink has been reached
 			for(int i = 0; i < h_frontier[0]; i++)
 				if(h_frontier[i + 1] == (N - 1)) {
 					augFound = true;
 					break;
 				}
 
-			//lauch kernel : launch threads to update frontier_len, visited and frontier in gpu local mem
-			s=clock();
+			//Call to kernels
+			s = clock();
 			kernel<<<h_frontier[0], N>>>(d_adj_mat,N,d_visited,d_frontier, d_new_frontier, d_par_mat, d_cap_mat, d_cap_max_mat);
-
-			k2<<<1,1>>>(N, d_visited,d_frontier, d_new_frontier);
-			e=clock();
-			t+=double(e-s);
+			k2<<<1, 1>>>(N, d_visited,d_frontier, d_new_frontier);
+			e = clock();
+			t += double(e - s);
 
 			cudaMemcpy((void*) h_frontier, (void*) d_frontier, sizeof(int) * (N+1), cudaMemcpyDeviceToHost);
 		}
 
-		cudaMemcpy((void*) h_par_mat, (void*) d_par_mat, sizeof(bool) * N * N, cudaMemcpyDeviceToHost);
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				cout<<h_par_mat[i*N+j]<<" ";
-			}
-			cout<<endl;
-		}
-
 		if(augFound) {
-			cout<<"Augmented path found!"<<endl;
+			cout<<"\nAugmented path found!"<<endl;
 			cudaMemcpy((void*) h_par_mat, (void*) d_par_mat, sizeof(bool) * N * N, cudaMemcpyDeviceToHost);
+
+			cout<<"Parent matrix:"<<endl<<endl;
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					cout<<h_par_mat[i * N + j]<<" ";
+				}
+				cout<<endl;
+			}
 
 			//Find the augmented path
 			int* augPath = (int*)malloc(N*sizeof(int));
@@ -222,7 +209,7 @@ int main(int arg, char** argv){
 						bottleneck = freeCap;
 				}
 			}
-			cout<<"bottleneck: "<<bottleneck<<endl;
+			cout<<"\nBottleneck of augmented path: "<<bottleneck<<endl;
 
 			//Update capacities in h_cap_mat
 			for(int i = 0; i < N; i++) {
@@ -241,16 +228,15 @@ int main(int arg, char** argv){
 
 			cudaMemcpy((void*) d_cap_mat, (void*) h_cap_mat, sizeof(int)*N*N, cudaMemcpyHostToDevice);
 		} else {
-			cout<<"Completed Ford Fulkerson!\n";
+			cout<<"\nFord Fulkerson complete!\n";
 			break;
 		}
 	}
 
 	end = clock();
-	cout<<"queue through put: "<< cn<<endl;
-	cout << "parallel BFS uses " << double(end - start) << " us in total"<< endl;
-	cout << "kernel launching and computing uses " <<t<<" us"<<endl;
-	cout << "mem copy uses " <<double(end - start) - t<<" us"<<endl;
+	cout<<"\nTime taken to run complete parallel Ford Fulkerson algorithm: "<<double(end - start)<<"us"<<endl;
+	cout<<"Time taken to run kernel: "<<t<<"us"<<endl;
+	cout<<"Time taken for memcpy from host to device: "<<double(end - start) - t<<"us"<<endl;
 
 	return 0;
 }
